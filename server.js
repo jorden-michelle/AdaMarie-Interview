ecb2ffea13876be0443a14952fbe61dcfc7585f3
Jorden Ragelse <jordenragels3@gmail.comm>
2023-09-26 20:02:39 -0600

    update cors config

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:8081"
+  origin:'http://localhost:8080', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
 };
 
 app.use(cors(corsOptions));
